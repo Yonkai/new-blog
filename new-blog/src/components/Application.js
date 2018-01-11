3bb294e007cff0a018d7d320f66709d8529ea3bf
Frank <fgunsch.webdev@gmail.com>
2018-01-11 15:16:48 -0500

    First commit

---
+++ new-blog/src/components/Application.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Application extends Component {
+  render() {
+    return (
+      <div className="Application">
+        <AbstractBackground/>
+        <MainTitleFirstContainer/>
+        <FirstContentContainer/>
+      </div>
+    );
+  }
+}
+
+export default Application;
---
+++ new-blog/src/components/_FAKETEMPLATE.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class example extends React.Component{
+    constructor(){
+        super();
+        //getinitialState
+ 
+       //bound: this.mthd = this.mthd.bind(this);
+        
+        this.state = {
+
+        };
+    }
+
+    createFish(event){
+        //Method
+    }
+    render(){
+        return(
+         //JSX
+        )
+    }
+}
+
+export default example;
