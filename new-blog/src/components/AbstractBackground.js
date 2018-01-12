fad92f56f6c41881b78e75a5bebea046740ea006
Frank <fgunsch.webdev@gmail.com>
2018-01-12 13:30:44 -0500

    Clarified goals

--- new-blog/src/components/AbstractBackground.js
+++ new-blog/src/components/AbstractBackground.js
@@ -2,15 +2,42 @@ import React from 'react';
 
 class AbstractBackground extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.updateDimensions = this
+            .updateDimensions
+            .bind(this);
+            
+        this.state = {
+            dimensions: {width:null,height:null}
+        }
+    }
+
     componentDidMount() {
         const canvas = this.refs.canvas;
         const context = canvas.getContext("2d");
-        
-        /*Recreate sharded glass effect in canvas, add an event listener for a screen resize.
-         Should it be in state?
-        */
+        this.updateDimensions();
 
-      }
+        /*Recreate sharded glass effect in canvas, add an event listener for a screen resize.
+
+         1. Detect+Set Dimensions on initial rendering in React. Sets background.
+         2. During intial rendering, set a callback listener to recall the initial rendering code to update state and canvas. -loops around.-
+        */
+       
+
+    }
+
+    updateDimensions() {
+        this.setState({
+            dimensions: {
+                width:5,
+                height: 5
+            },
+        },function(){
+            console.log(this.state.dimensions.width,this.state.dimensions.height);
+        });
+    }
 
     render() {
         return (
