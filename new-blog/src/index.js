d7db805aa32937fb70bd17ac89a35e125b4f59f4
Frank <fgunsch.webdev@gmail.com>
2018-01-14 18:23:15 -0500

    CSS Properly loading in Create-react-app now.

--- new-blog/src/index.js
+++ new-blog/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/Application';
 import registerServiceWorker from './registerServiceWorker';
+import './CSS/App.css';
+
 
 ReactDOM.render(<Application />, document.getElementById('root'));
 registerServiceWorker();
